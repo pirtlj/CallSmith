9dee296446e181a80168217c4d14dee83aff2ef5
pirtlj <pirtlj@gmail.com>
2011-09-21 21:47:35 -0700

    asset bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ CallSmith::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
@@ -43,7 +45,7 @@ CallSmith::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( contacts.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
