645e397bfd415def54280a3cb232468da194a1ca
pirtlj <pirtlj@gmail.com>
2011-09-22 16:24:49 -0700

    work work work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ CallSmith::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( contacts.css, jquery.css )
+  config.assets.precompile += %w(contacts.css)
+  config.assets.precompile += %w(jquery.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
