aee38802bca94822fec746cee18b4fca33966b4c
Joseph Pirtle <pirtlj@gmail.com>
2011-10-18 16:36:38 -0700

    work work work

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -40,7 +40,8 @@ class CallsController < ApplicationController
       :from => '+12064037411',
       :to => '+12064037411',
       :url => handler_call_url(@call, :format => :twiml, :auth_token => current_user.authentication_token),
-      :statuscallback  => root_url()
+      :StatusCallback  => root_url(),
+      :StatusCallbackMethod => "get"
     )
     flash[:notice] = "Dialing..."
     redirect_to root_url()
