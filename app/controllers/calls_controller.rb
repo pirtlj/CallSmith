ebeafbe62c909bcbb186807591fdc8c74f0205b9
Joseph Pirtle <pirtlj@gmail.com>
2011-10-18 16:14:23 -0700

    work work work

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -39,7 +39,7 @@ class CallsController < ApplicationController
     @client.account.calls.create(
       :from => '+12064037411',
       :to => '+12064037411',
-      :url => handler_call_url(@call, :format => :twiml, :auth_token => current_user.authentication_token)
+      :url => handler_call_url(@call, :format => :twiml, :auth_token => current_user.authentication_token),
       :statuscallback  => root_url()
     )
     flash[:notice] = "Dialing..."
