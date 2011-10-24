be856c9f11d72fae01e3b3117ad4284f9a672e65
Joseph Pirtle <pirtlj@gmail.com>
2011-10-23 20:29:29 -0700

    work work work

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -111,7 +111,7 @@ class CallsController < ApplicationController
 
     respond_to do |format|
       if @call.save
-        format.html { redirect_to @call, notice: 'Call was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Call was successfully created.' }
         format.json { render json: @call, status: :created, location: @call }
       else
         format.html { render action: "new" }
